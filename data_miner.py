b548acacab0b7759e23ab163b66bc714a2d4f5ec
Sam Magura <srmagura@gmail.com>
2012-01-25 18:52:37 -0500

    Fix I_step and do a little cleanup

--- data_miner.py
+++ data_miner.py
@@ -27,17 +27,10 @@ class Entry:
 
 
 if __name__ == '__main__':
-##    n_nodes = 40
-##    n_edges = 20
-##
-##    n_simulations = 200
-##    n_steps = 50
-##    n_iterations = 100
 
     try:
         n_nodes = input("How many nodes? ")
         n_edges = input("How many edges? ")
-##        I = float(input("What initial I value? "))
         I = 0.0
         n_steps = input("Use how many different I values? ")
         n_simulations = input("Average how many simulations per I value? ")
@@ -51,7 +44,7 @@ What model do you want to use? ")
     except ValueError:
         print "Please enter integers.\n"
 
-    I_step = 1.0 / float(n_simulations)
+    I_step = 1.0 / float(n_steps)
 
     output1 = 'I\tgc_n_nodes\tgc_diameter\tn_components\n'
     output2 = 'Step\tgc_n_nodes\tgc_diameter\tn_components\n'
@@ -61,10 +54,13 @@ What model do you want to use? ")
         gc_n_nodes_list = []
         gc_diameter_list = []
         n_components_list = []
+
         for j in range(n_simulations):
             graph = networkx.gnm_random_graph(n_nodes, n_edges)
+
             for j in range(n_iterations):
                 simulation.iterate(graph, I, model)
+
             components = networkx.connected_component_subgraphs(graph)
             gc_n_nodes_list.append(components[0].number_of_nodes())
             gc_diameter_list.append(networkx.diameter(components[0]))
@@ -82,16 +78,18 @@ What model do you want to use? ")
         output1 += '%s\n' % entry
         I += I_step
 
-#Comparing iterations
+    #Comparing iterations
     G = networkx.gnm_random_graph(n_nodes, n_edges)
     for k in range(n_iterations):
         simulation.iterate(G, I, model)
         components = networkx.connected_component_subgraphs(G)
+   
         iterBuf = IterEntry()
         iterBuf.step = k
         iterBuf.gc_n_node = components[0].number_of_nodes()
         iterBuf.gc_diameter = networkx.diameter(components[0])
         iterBuf.n_components = len(components)
+   
         output2 += '%s\n' % iterBuf
         
     outfile1 = open('avgResults_gnm_%dm_%dn_%de.dat' % (model, n_nodes, n_edges), 'w')
