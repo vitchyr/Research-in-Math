1a22a4606047f212e0204f862031725f7819af35
Sam Magura <srmagura@gmail.com>
2012-02-19 13:21:01 -0500

    Fix big mistake in simulation.py
    iterate was doing nothing to the graph if the node it chose as v0 had degree ==
    0.
    
    Also lots cleanup

--- simulation.py
+++ simulation.py
@@ -1,56 +1,44 @@
 import random
 import copy
-import math
+import sys
 
 import networkx
-import matplotlib.pyplot
-
-##Model Options
-##1. Pick v_0 random & Break with probability
-##2. Pick v_0 randomly & Rewire with probability
-##3. Pick v_0 via e_0 & Break with probability
-##4. Pick v_0 via e_0 & Rewire with probability
+import matplotlib
 
 def weighted_choice(weights):
     totals = {}
     running_total = 0
 
-    for key, weight in weights.iteritems():
-        running_total += weight
+    for key in weights:
+        running_total += weights[key]
         totals[key] = running_total
 
     rnd = random.random() * running_total
-    for key, total in totals.iteritems():
-        if rnd < total:
+
+    for key in totals:
+        if rnd < totals[key]:
             return key
 
-def absolute_choice(weights):
-    return min(weights, key=weights.get)
-
-#Unnormalized breaking probability
 def break_pr_function(I, d_neighbor, avg_degree):
-    pr = float(I) / d_neighbor + float(1 - I) / avg_degree
-    return pr
+    return float(I) / d_neighbor + float(1 - I) / avg_degree
 
 def rewire_pr_function(I, d_someNode, avg_degree):
-    pr = I * d_someNode + (1-I) * avg_degree
-    return pr
+    return I * d_someNode + (1-I) * avg_degree
 
 def get_avg_degree(graph):
     return 2 * graph.number_of_edges() / float(graph.number_of_nodes()) 
 
-def breakWeighted(graph, I, node):
+def break_weighted(graph, I, node):
     weights = {}
-    avg_degree = get_avg_degree(graph)
 
     for neighbor in graph.neighbors(node):    
         weights[neighbor] = break_pr_function(I, graph.degree(neighbor), 
-            avg_degree)   
+            graph.avg_degree)   
 
     node_to_defriend = weighted_choice(weights)
     graph.remove_edge(node, node_to_defriend)
 
-def rewireRandomly(graph, node):
+def rewire_randomly(graph, node):
     other_nodes = copy.copy(graph.nodes())
     other_nodes.remove(node)
     while True:
@@ -61,69 +49,71 @@ def rewireRandomly(graph, node):
             graph.add_edge(node, new_node)
             break
 
-def rewireWeighted(graph, node, I):
-    graph_copy = copy.copy(graph.nodes()) #shallow copy of all potential nodes to make a new edge with
-    graph_copy.remove(node)
-    
-    for potentialNode in graph_copy: #remove nodes that already have an edge with *node*
-        if graph.has_edge(node, potentialNode):
-            graph_copy.remove(potentialNode)
+def rewire_weighted(graph, node, I):
+    nodes_copy = copy.copy(graph.nodes()) 
+    nodes_copy.remove(node)
+   
+    #Remove nodes already adjacent to v0 
+    for potential_node in nodes_copy: 
+        if graph.has_edge(node, potential_node):
+            nodes_copy.remove(potential_node)
             
-    #Probability Setup
     weights = {}
-    avg_degree = get_avg_degree(graph)
     
-    for someNode in graph_copy:    
-        weights[someNode] = rewire_pr_function(I, graph.degree(someNode), 
-            avg_degree)
-    #Choose new node to connect to
+    for some_node in nodes_copy:    
+        weights[some_node] = rewire_pr_function(I, graph.degree(some_node), 
+            graph.avg_degree)
+
+    #Select new node
     new_node = weighted_choice(weights)      
     graph.add_edge(node, new_node)
 
     return weights[new_node]
 
-def iterate(graph, I, m): #m is the model number used (1-4)
+def iterate(graph, I, model_no):
+    #avg_degree is actually constant - just make sure it's set
+    graph.avg_degree = get_avg_degree(graph)
 
-    if m<3:
+    while True:
         node = random.choice(graph.nodes())
-    else:
-        node = random.choice(random.choice(graph.edges()))
 
-    #Checks:
-    if graph.degree(node) == 0:
-        return
-    if len(graph.neighbors(node)) == 0: #double check (didn't work without sometimes)
-        return
+        if graph.degree(node) > 1:
+            break
 
-    #Rewire
-    if m%2: #break weighted rewire randomly
-        breakWeighted(graph, I, node)
-        rewireRandomly(graph, node)
+    #break-function model
+    if model_no == 1: 
+        break_weighted(graph, I, node)
+        rewire_randomly(graph, node)
         
-    else: #break evenly rewired weighted
+    #rewire-function model
+    else: 
         old_node = random.choice(graph.neighbors(node)) 
         graph.remove_edge(node, old_node) 
 
         #this stuff for markov.py
-        R_new_node = rewireWeighted(graph, node, I)
+        R_new_node = rewire_weighted(graph, node, I)
         R_old_node = rewire_pr_function(I, graph.degree(old_node), 
             get_avg_degree(graph))
 
-        return R_new_node / R_old_node
+        if R_old_node != 0:
+            return R_new_node / R_old_node
 
 
-##if __name__ == '__main__':
-##    #Set this stuff manually
-##    try:
-##        n_nodes = input("How many nodes? ")
-##        n_edges = input("Make an edge withedges? ")
-##        I = float(input("What I value? "))
-##    graph = networkx.erdos_renyi_graph(40, .025)
-##    times = 100
-##    I = 1
-##
-##    for i in range(times):
-##        iterate(graph, I)
-##
-##    networkx.draw(graph)
-##    matplotlib.pyplot.show()
+if __name__ == '__main__':
+    try:
+        n_nodes = input("How many nodes? ")
+        n_edges = input("How many edges? ")
+        I = float(input("What I value? "))
+        model_no = input("Model number? (1/2) ")
+        times = input("How many iterations? ")
+    except:
+        print("Invalid input. Exiting...")
+        sys.exit(1)
+
+    graph = networkx.gnm_random_graph(n_nodes, n_edges)
+
+    for i in range(times):
+        iterate(graph, I, model_no)
+
+    networkx.draw(graph)
+    matplotlib.pyplot.show()
