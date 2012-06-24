5668e1e260a3f7a7ecaa9e490cfeac06123d2b7e
Sam Magura <srmagura@gmail.com>
2012-06-24 15:35:36 -0400

    updates to gc_dm.py

--- opinion_model/gc_dm.py
+++ opinion_model/gc_dm.py
@@ -2,30 +2,34 @@ import networkx
 import model
 
 if __name__ == '__main__':
-    n_min = 200
-    n_step = 25
-    n_max = 800
+    k_min = .6
+    k_step = .2
+    k_max = 2.0
 
-    k = 4
-    D = 1
-    times = 5*10**4
+    n = 400
+    D = 3
+    times = 0*5*10**4
     stats_size = 50
 
     outstring = ''
-    for n in xrange(n_min, n_max, n_step):
+    k = k_min
+    
+    while k < k_max + .01:
+        print 'k = {0}'.format(k)
 
-        diam_total = 0
+        gc_total = 0
         for j in xrange(stats_size):
             G = model.make_graph(n, .5 * n * k, D)
 
             for i in xrange(times):
                 model.iterate(G)
 
-            gc = networkx.connected_component_subgraphs(G)[0]
-            diam_total += networkx.diameter(gc)
+            gc = networkx.connected_components(G)[0]
+            gc_total += len(gc) / float(n)
 
-        outstring += '{0}\t{1}\n'.format(n, diam_total / float(stats_size))
+        outstring += '{0}\t{1}\n'.format(k, gc_total / float(stats_size))
+        k += k_step
 
-    filename = 'opdiameter_{0}_{1}.dat'.format(k, D)
+    filename = 'opgc_{0}_{1}.dat'.format(n, D)
     outfile = open(filename, 'w')
     outfile.write(outstring)
