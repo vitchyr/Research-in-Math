029d41cb51413b401afaed369921a7043dcb4234
Vitchyr Pong <vitchyr@gmail.com>
2012-07-12 11:13:38 -0400

    added the wrong possion explanation, and made gc_dm use the new model.py

--- opinion_model/gc_dm.py
+++ opinion_model/gc_dm.py
@@ -8,18 +8,18 @@ if __name__ == '__main__':
 
     n = 400
     D = 3
-    times = 0*5*10**4
+    times = 5*10**4
     stats_size = 50
 
-    outstring = ''
+    outstring = 'mean degree\tGC size'
     k = k_min
     
-    while k < k_max + .01:
+    while k <= k_max:
         print 'k = {0}'.format(k)
 
         gc_total = 0
         for j in xrange(stats_size):
-            G = model.make_graph(n, .5 * n * k, D)
+            G = model.make_opinion_graph(n, .5 * n * k, D)
 
             for i in xrange(times):
                 model.iterate(G)
@@ -28,8 +28,10 @@ if __name__ == '__main__':
             gc_total += len(gc) / float(n)
 
         outstring += '{0}\t{1}\n'.format(k, gc_total / float(stats_size))
+        print '{0}\t{1}\n'.format(k, gc_total / float(stats_size))
         k += k_step
 
     filename = 'opgc_{0}_{1}.dat'.format(n, D)
     outfile = open(filename, 'w')
     outfile.write(outstring)
+    outfile.close()
