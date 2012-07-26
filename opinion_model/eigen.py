f022f8792013108504001eec089ccf9843313ac2
Sam Magura <srmagura@gmail.com>
2012-07-26 18:17:52 -0400

    fix eigen

--- opinion_model/eigen.py
+++ opinion_model/eigen.py
@@ -8,23 +8,33 @@ if __name__ == '__main__':
     n = 500
     b = .000006662 
 
-    ticks = 1000
+    ticks = 5
 
-    D = 1
+    D = 3
     alpha = 2
 
-    type_ids = np.ndindex(tuple([ticks]* D * 2))
-    M = np.zeros([ticks] * D * 2)
+    print 'Number of types: {}'.format(ticks**(D))
+    print 'Number of elements in matrix: {}'.format(ticks**(D*2))
+    M = np.zeros([ticks**D, ticks**D])
+    registry = {}
     
-    for index in type_ids:
+    next_id = 0
+
+    for index in np.ndindex(tuple([ticks] * D)):
+        i = index[:D]
+        registry[i] = next_id 
+        next_id += 1
+
+    for index in np.ndindex(tuple([ticks]* D * 2)):
         i = index[:D]
         j = index[D:]
 
         if i != j:
-            pos_i = [float(_i) / (ticks - 1) for _i in i] 
+            pos_i = [float(_i) / (ticks - 1) for _i in i]
             pos_j = [float(_j) / (ticks - 1) for _j in j]
 
-            M[i, j] = b / (b + model.distance(None, pos_i, pos_j)) 
+            M[registry[i], registry[j]] = \
+                b / (b + model.distance(None, pos_i, pos_j)**alpha) 
 
     eigvals = scipy.linalg.eigvals(M) 
     print max(eigvals)
