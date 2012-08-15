fe530c62116451ca3e83d997b1ff3f1a74c597a2
Vitchyr Pong <vitchyr@gmail.com>
2012-08-15 11:15:39 -0400

    more data analysis

--- degree_model/data_analysis.py
+++ degree_model/data_analysis.py
@@ -1,10 +1,6 @@
 import matplotlib.pyplot as plt
 import math
 
-xlist = []
-ylist = []
-filename = ""
-
 for number in range(0,15,5):
     formatString = "%0.1f" % (number/10.0)
     filename = "data/stats_2000n_"+formatString+"00000th_"+str(int(number/10) + 1)+"00000times_0.600000kmin_0.200000kstep_2.000000kmax_10statsize.dat"
@@ -20,22 +16,19 @@ for number in range(0,15,5):
         y.append(float(datum[1]))
     
     f.close()
-    xlist.append(x)
-    ylist.append(y)
 
     if number == 0:
-        plt.scatter(x,y, c='r', marker='o', label="theta = {0}".format(formatString))
+        plt.scatter(x,y, c='r', marker='s', label="theta = {0}".format(formatString))
     elif number == 5:
-        plt.scatter(x,y, c='g', marker='s', label="theta = {0}".format(formatString))
+        plt.scatter(x,y, c='g', marker='^', label="theta = {0}".format(formatString))
     else:
-        plt.scatter(x,y, c='b', marker='^', label="theta = {0}".format(formatString))
-    plt.xlabel(headers[0])
-    plt.ylabel(headers[1])
+        plt.scatter(x,y, c='b', marker='o', label="theta = {0}".format(formatString))
 
-    ##plt.axis([0,5500, 0,700000])
-    ##ax = plt.gca()    
-    ##ax.set_autoscale_on(False)
+plt.title("Size of Largest Component: theta = 0, 0.5, 1")
+plt.xlabel("Average Degree")
+plt.ylabel("Fraction of Vertices in Largest Component")
+##plt.xlim(0, 2)
+##plt.ylim(0,1)
 plt.legend(loc=4)
-
 plt.savefig("data/degree_model_lc_2000n.pdf")
 
